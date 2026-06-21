2c562a7a0cbca71f822639c19e5aa272d1c08576
primes-banque-fr <tchammamadi99@gmail.com>
2026-06-21 10:58:59 +0100

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,36 @@
+const pino = require('pino');
+‎
+‎class Logger {
+‎    constructor(name) {
+‎        this.name = name;
+‎        this.logger = pino({
+‎            level: 'info',
+‎            transport: {
+‎                target: 'pino-pretty',
+‎                options: {
+‎                    colorize: true,
+‎                    translateTime: 'HH:MM:ss',
+‎                    ignore: 'pid,hostname'
+‎                }
+‎            }
+‎        });
+‎    }
+‎
+‎    info(msg) {
+‎        this.logger.info(`[${this.name}] ${msg}`);
+‎    }
+‎
+‎    success(msg) {
+‎        this.logger.info(`\x1b[32m[${this.name}] ✅ ${msg}\x1b[0m`);
+‎    }
+‎
+‎    error(msg) {
+‎        this.logger.error(`[${this.name}] ❌ ${msg}`);
+‎    }
+‎
+‎    warn(msg) {
+‎        this.logger.warn(`[${this.name}] ⚠️ ${msg}`);
+‎    }
+‎}
+‎
+‎module.exports = Logger;
